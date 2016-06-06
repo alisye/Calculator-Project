a8b64654ed85ac234e0fb9323b8b2433be578e14
alisye <muntasir20@gmail.com>
2016-06-06 18:43:34 -0400

    Add files via upload

---
+++ calculator_runner.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class killer {
+	public static void main(String[] args){
+
+		calculator a = new calculator();
+		a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		a.setSize(250,270);
+		a.setVisible(true);
+	}
+	
+}
+
