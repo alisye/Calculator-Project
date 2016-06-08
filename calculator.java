ab1045b2b51a7c651031a77d360e055bd9f06af0
alisye <muntasir20@gmail.com>
2016-06-08 16:21:32 -0400

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -6,10 +6,14 @@ public class calculator extends JFrame{
 	
 	private JTextField answerDisplay;
 	private JButton one, two, three, four, five, six, seven, eight, nine, zero, pi, add,
-	sub, mul, div, equals, clear, negative, decimal, sin, cos, tan;
+	sub, mul, div, equals, clear, negative, decimal, sin, cos, tan, sq, sqrt, e, ln, log, E, 
+	exp, inv;
+	private JRadioButton rad, deg;
+	private ButtonGroup group;
 	private double answer;
 	private String fnum, snum;
-	private boolean opPress = false, equalPress = false;
+	private boolean opPress = false, equalPress = false, inversePress = false, 
+	radPress = true;
 	private JPanel ControlPanel;
 	private char op = ' ';
 	
@@ -17,7 +21,7 @@ public class calculator extends JFrame{
 		
 		super("Calculator");
 		ControlPanel = new JPanel();
-		ControlPanel.setBackground(Color.gray);
+		ControlPanel.setBackground(Color.white);
 		ControlPanel.setLayout(new FlowLayout());
 		
 		answerDisplay = new JTextField(null, 18);
@@ -43,21 +47,39 @@ public class calculator extends JFrame{
 		negative = new JButton("neg");
 		decimal = new JButton(".");
 		pi = new JButton("π");
+		e = new JButton("e");
 		sin = new JButton("sin");
 		cos = new JButton("cos");
 		tan = new JButton("tan");
+		sq = new JButton("x²");
+		sqrt = new JButton("√");
+		ln = new JButton("ln");
+		log = new JButton("log");
+		E = new JButton("E");
+		exp = new JButton("exp");
+		inv = new JButton("Inv");
+		
+		group = new ButtonGroup();
+		rad = new JRadioButton("rad", true);
+		deg = new JRadioButton("deg", false);
+		group.add(rad);
+		group.add(deg);
 		
 		ControlPanel.add(one); ControlPanel.add(two); ControlPanel.add(three);
 		ControlPanel.add(four);ControlPanel.add(five);ControlPanel.add(six);
 		ControlPanel.add(seven);ControlPanel.add(eight);ControlPanel.add(nine);
-		ControlPanel.add(zero);ControlPanel.add(pi); ControlPanel.add(add);
-		ControlPanel.add(sub);ControlPanel.add(mul);ControlPanel.add(div); 
+		ControlPanel.add(zero);ControlPanel.add(pi); ControlPanel.add(e);
+		ControlPanel.add(ln);ControlPanel.add(log);ControlPanel.add(add);
+		ControlPanel.add(sub);ControlPanel.add(mul);ControlPanel.add(div);
+		ControlPanel.add(sq);ControlPanel.add(sqrt);ControlPanel.add(exp);
 		ControlPanel.add(sin);ControlPanel.add(cos);ControlPanel.add(tan);
-		ControlPanel.add(clear);ControlPanel.add(decimal);ControlPanel.add(negative);
-		ControlPanel.add(equals);
+		ControlPanel.add(E);ControlPanel.add(clear);ControlPanel.add(decimal);
+		ControlPanel.add(negative);ControlPanel.add(inv);ControlPanel.add(equals);
+		ControlPanel.add(rad);ControlPanel.add(deg);
 		
 		Numbers n = new Numbers();
 		Calc c = new Calc();
+		Angs a = new Angs();
 		
 		one.addActionListener(n);
 		two.addActionListener(n);
@@ -81,7 +103,18 @@ public class calculator extends JFrame{
 		sin.addActionListener(c);
 		cos.addActionListener(c);
 		tan.addActionListener(c);
+		sq.addActionListener(c);
+		sqrt.addActionListener(c);
+		e.addActionListener(n);
+		ln.addActionListener(c);
+		log.addActionListener(c);
+		E.addActionListener(c);
+		exp.addActionListener(c);
+		inv.addActionListener(c);
 		
+		rad.addItemListener(a);
+		deg.addItemListener(a);
+
 		this.setContentPane(ControlPanel);
 	}
 	private class Numbers implements ActionListener{
@@ -295,6 +328,23 @@ public class calculator extends JFrame{
 					}else{}
 				}
 			}
+			else if (src.equals(e)){
+				if (opPress == false && equalPress == false){
+					if (fnum == null){
+						fnum = Double.toString(Math.E);
+					}else{}
+				}else if (equalPress == true){
+					fnum = Double.toString(Math.E);
+					equalPress = false;
+					snum = null;
+					opPress = false;
+					op = ' ';
+				}else{
+					if (snum == null){
+						snum = Double.toString(Math.E);
+					}else{}
+				}
+			}
 			else {
 				if (opPress == false && equalPress == false){
 					if (fnum == null){
@@ -344,25 +394,41 @@ public class calculator extends JFrame{
 			case '*': {answer = d1 * d2;
 			        break;}
 			case '/': {answer = d1 / d2;
-			         break;}
+			        break;}
+			case 'E': {answer = d1*Math.pow(10,d2);
+			        break;}
+			case 'P': {answer = Math.pow(d1, d2);
+			        break;}
 			}
+			
 			String ans = Double.toString(answer);
 			answerDisplay.setText(ans);
 			fnum = ans;
 		}
 		
+		private String converter(String d){
+			double l = Double.parseDouble(d);
+			double q = l*(Math.PI/180);
+			return Double.toString(q);
+		}
+		
 		public void actionPerformed(ActionEvent event){
 			JButton src = (JButton) event.getSource();
 			if (src.equals(add)){
-				if (fnum != null && snum == null && opPress == false){
+				if (fnum != null && snum == null && opPress == false && equalPress == false){
 					opPress = true;
 					op = '+';
 				}else if (opPress == true && equalPress == false){
 					solver();
 					snum = null;
 					op = '+';
+				}else {
+					snum = null;
+					op = '+';
+					opPress = true;
+					equalPress = false;
 				}
-				else{}
+				
 			}
 			if (src.equals(sub)){
 				if (fnum != null && snum == null){
@@ -373,7 +439,12 @@ public class calculator extends JFrame{
 					snum = null;
 					op = '-';
 				}
-				else {}
+				else {
+					snum = null;
+					op = '-';
+					opPress = true;
+					equalPress = false;
+				}
 			}
 			if (src.equals(mul)){
 				if (fnum != null && snum == null){
@@ -384,7 +455,12 @@ public class calculator extends JFrame{
 					snum = null;
 					op = '*';
 				}
-				else {}
+				else {
+					snum = null;
+					op = '*';
+					opPress = true;
+					equalPress = false;
+				}
 			}
 			if (src.equals(div)){
 				if (fnum != null && snum == null){
@@ -395,63 +471,217 @@ public class calculator extends JFrame{
 					snum = null;
 					op = '/';
 				}
-				else {}
+				else {
+					snum = null;
+					op = '/';
+					opPress = true;
+					equalPress = false;
+				}
 			}
 			if (src.equals(sin)){
-				if (fnum != null && snum == null && equalPress == false){
+				if (fnum != null && snum == null && equalPress == false 
+						&& inversePress == false){
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.sin(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					answerDisplay.setText(fnum);
-				}else if (fnum != null && snum != null && equalPress == false){
+				}else if (fnum != null && snum != null && equalPress == false 
+						&& inversePress == false){
 					solver();
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.sin(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					snum = null;
 					opPress = false;
 					op = ' ';
 					answerDisplay.setText(fnum);
-				}else if (equalPress == true){
+				}else if (equalPress == true && inversePress == false){
 					snum = null;
 					equalPress = false;
 					opPress = false;
 					op = ' ';
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.sin(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum == null && equalPress == false 
+						&& inversePress == true){
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.asin(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false 
+						&& inversePress == true){
+					solver();
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.asin(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true && inversePress == true){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.asin(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					answerDisplay.setText(fnum);
 				}
 			}
 			if (src.equals(cos)){
-				if (fnum != null && snum == null && equalPress == false){
+				if (fnum != null && snum == null && equalPress == false 
+						&& inversePress == false){
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.cos(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					answerDisplay.setText(fnum);
-				}else if (fnum != null && snum != null && equalPress == false){
+				}else if (fnum != null && snum != null && equalPress == false
+						&& inversePress == false){
 					solver();
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.cos(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					snum = null;
 					opPress = false;
 					op = ' ';
 					answerDisplay.setText(fnum);
-				}else if (equalPress == true){
+				}else if (equalPress == true && inversePress == false){
 					snum = null;
 					equalPress = false;
 					opPress = false;
 					op = ' ';
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.cos(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum == null && equalPress == false 
+						&& inversePress == true){
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.acos(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false
+						&& inversePress == true){
+					solver();
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.acos(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true && inversePress == true){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.acos(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					answerDisplay.setText(fnum);
 				}
 			}
 			if (src.equals(tan)){
-				if (fnum != null && snum == null && equalPress == false){
+				if (fnum != null && snum == null && equalPress == false 
+						&& inversePress == false){
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.tan(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
 					answerDisplay.setText(fnum);
-				}else if (fnum != null && snum != null && equalPress == false){
+				}else if (fnum != null && snum != null && equalPress == false
+						&& inversePress == false){
 					solver();
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
 					Double angle = Math.tan(Double.parseDouble(fnum));
 					fnum = Double.toString(angle);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true && inversePress == false){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.tan(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum == null && equalPress == false 
+						&& inversePress == true){
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.atan(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false
+						&& inversePress == true){
+					solver();
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.atan(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true && inversePress == true){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					if (radPress == false){
+						fnum = converter(fnum);
+					}
+					Double angle = Math.atan(Double.parseDouble(fnum));
+					fnum = Double.toString(angle);
+					answerDisplay.setText(fnum);
+				}
+			}
+			if (src.equals(sq)){
+				if (fnum != null && snum == null && equalPress == false){
+					Double base = Math.pow(Double.parseDouble(fnum), 2);
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false){
+					solver();
+					Double base = Math.pow(Double.parseDouble(fnum), 2);
+					fnum = Double.toString(base);
 					snum = null;
 					opPress = false;
 					op = ' ';
@@ -461,9 +691,127 @@ public class calculator extends JFrame{
 					equalPress = false;
 					opPress = false;
 					op = ' ';
-					Double angle = Math.tan(Double.parseDouble(fnum));
-					fnum = Double.toString(angle);
+					Double base = Math.pow(Double.parseDouble(fnum), 2);
+					fnum = Double.toString(base);
 					answerDisplay.setText(fnum);
+				}
+			}
+			if (src.equals(sqrt)){
+				if (fnum != null && snum == null && equalPress == false){
+					Double base = Math.sqrt(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false){
+					solver();
+					Double base = Math.sqrt(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					Double base = Math.sqrt(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}
+			}
+			if (src.equals(ln)){
+				if (fnum != null && snum == null && equalPress == false){
+					Double base = Math.log(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false){
+					solver();
+					Double base = Math.log(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					Double base = Math.log(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}
+			}
+			if (src.equals(log)){
+				if (fnum != null && snum == null && equalPress == false){
+					Double base = Math.log10(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}else if (fnum != null && snum != null && equalPress == false){
+					solver();
+					Double base = Math.log10(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					snum = null;
+					opPress = false;
+					op = ' ';
+					answerDisplay.setText(fnum);
+				}else if (equalPress == true){
+					snum = null;
+					equalPress = false;
+					opPress = false;
+					op = ' ';
+					Double base = Math.log10(Double.parseDouble(fnum));
+					fnum = Double.toString(base);
+					answerDisplay.setText(fnum);
+				}
+			}
+			if (src.equals(E)){
+				if (fnum != null && snum == null && opPress == false && equalPress == false){
+					opPress = true;
+					op = 'E';
+				}else if (opPress == true && equalPress == false){
+					solver();
+					snum = null;
+					op = 'E';
+				}else {
+					snum = null;
+					op = 'E';
+					opPress = true;
+					equalPress = false;
+				}
+				
+			}
+			if (src.equals(exp)){
+				if (fnum != null && snum == null && opPress == false && equalPress == false){
+					opPress = true;
+					op = 'P';
+				}else if (opPress == true && equalPress == false){
+					solver();
+					snum = null;
+					op = 'P';
+				}else {
+					snum = null;
+					op = 'P';
+					opPress = true;
+					equalPress = false;
+				}
+				
+			}
+			if (src.equals(inv)){
+				if (inversePress == false){
+					inversePress = true;
+					sin.setText("sin‾¹");
+					cos.setText("cos‾¹");
+					tan.setText("tan‾¹");
+					inv.setOpaque(true);
+					inv.setBackground(Color.MAGENTA);
+				}
+				else{
+					inversePress = false;
+					sin.setText("sin");
+					cos.setText("cos");
+					tan.setText("tan");
+					inv.setOpaque(false);
 				}
 			}
 			if (src.equals(equals)){
@@ -517,6 +865,23 @@ public class calculator extends JFrame{
 			}
 		}
 	}
+	private class Angs implements ItemListener{
+		
+		public void itemStateChanged(ItemEvent event){
+			
+			if (rad.isSelected()){
+				radPress = true;
+			}else if (deg.isSelected()){
+				radPress = false;
+			}
+		}
+	}
+	public static void main(String[] args){
+
+		calculator a = new calculator();
+		a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		a.setSize(250,450);
+		a.setResizable(false);
+		a.setVisible(true);
+	}
 }
-
-
